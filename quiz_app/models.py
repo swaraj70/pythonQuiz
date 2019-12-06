1f1f87372e23cfc07c61ce13b7f675353895b060
Swaraj Papadkar <apple@Apples-MacBook-Pro.local>
2019-12-06 23:44:56 +0530

    Quiz db changes

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -4,5 +4,22 @@ from django.db import models
 
 class Quiz(models.Model):
     q_name = models.CharField(max_length=30)
+    desc = models.CharField(max_length=100)
+    
     def __str__(self):
         return self.q_name
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    label = models.CharField(max_length=1000)
+    
+    def __str__(self):
+        return self.label
+        
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+    is_correct = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.text
--- quiz_app/views.py
+++ quiz_app/views.py
@@ -6,7 +6,8 @@ def home(request):
     return render(request, 'base.html')
 
 def discover(request):
-    return render(request, 'discover.html')
+    all_quiz = Quiz.objects.all()
+    return render(request, 'discover.html', {'all_quiz' : all_quiz})
 
 def quiz(request, quiz_id):
     quiz_obj = Quiz.objects.get(pk=quiz_id)
