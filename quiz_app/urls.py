c2c52fc399f5a3094dfdc6256f04595097b3c1b0
Swaraj Papadkar <apple@Apples-MacBook-Pro.local>
2019-12-09 15:02:16 +0530

    quest.html

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from quiz_app import views
 
 urlpatterns = [
-    path('<qname>', views.quiz, name='quiz')
+    path('<qname>', views.quiz, name='quiz'),
+    path('<quiz_name>/<ques_no>', views.basic_quest, name='basic_quest'),
 ]
--- quiz_app/views.py
+++ quiz_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Quiz
+from .models import Quiz, Question, Answer
+from .forms import QuestionForm
 
 # Create your views here.
 def home(request):
@@ -12,3 +13,8 @@ def discover(request):
 def quiz(request, qname):
     quiz_obj = Quiz.objects.get(q_name = qname)
     return render(request, 'quiz.html', {'quiz_obj' : quiz_obj})
+
+def basic_quest(request, quiz_name, ques_no):
+    question = Question.objects.get(id = ques_no)
+    answers = Answer.objects.filter(question = ques_no)
+    return render(request, 'quest.html', {'question' : question, 'answers' : answers})
